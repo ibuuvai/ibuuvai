7b5be52f763c7f73b8aac23fa919e09d9efb6ad6
vai <116262009+ibuuvai@users.noreply.github.com>
2025-10-19 23:12:03 +0100

    Update florilegium.ts

--- src/lib/data/florilegium.ts
+++ src/lib/data/florilegium.ts
@@ -2,6 +2,30 @@ export type FlorilegiumEntry = { title: string; content: string };
 
 export const entries: FlorilegiumEntry[] = [
 	{
+		title: `spring comes; I'm bruised and broken`,
+		content: `This must be love because I don't know what else to call it.
+How can I exemplify "you and me"?
+I bite down and taste the sand in my mouth—
+Then I kneel in evening-wet grass and rehearse living.
+“Isn’t it too early to end?” I ask my restless heart.
+I kept the pain inside until it learned your name.
+Keep me, even broken—
+I am not finished ending yet.`
+	},
+	{
+		title: `this hole in my chest, can't hold any love`,
+		content: `Emotions and feelings are useless to keep.
+The tears that had fallen were not mine to weep.
+The kindness and warmth, I can't feel them at all.
+The hands that are offered, I'm scared that I'll fall`
+	},
+	{
+		title: `the space between us`,
+		content: `Can't we entertain the idea?
+You rejected it so quickly, my heart sank.
+I repeat your words like I rehearsed it.`
+	},
+	{
 		title: 'something like that',
 		content: `Whiskey shall be my undoing;
 I've never known a poison so gentle in its cruelty.
@@ -25,7 +49,9 @@ It’s a small map; it leads everywhere I cannot go.`
 	},
 	{
 		title: 'are you okay?',
-		content: `I am—when you don’t look. When you do, I am tormented by the wish to answer honestly.`
+		content: `I am—when you don’t look.
+When you do,
+I am tormented by the wish to answer honestly.`
 	},
 	{
 		title: ':)',
@@ -47,9 +73,9 @@ We will never become lies, right?`
 	{
 		title: 'museum of almosts',
 		content: `Every night I curate the exhibits:
-		the hug I didn’t take,
-		the train I watched depart,
-		the life that bowed and left without applause.`
+the hug I didn’t take,
+the train I watched depart,
+the life that bowed and left without applause.`
 	},
 	{
 		title: 'weather report for the soul',
@@ -60,16 +86,16 @@ Midnight rain: I remember too much.`
 	{
 		title: 'white, once',
 		content: `how many colours have we been stained with?
-		i remember a white ocean i walked—each step darkening,
-		until the path became ink.
-		if i keep going, will it write me out?`
+i remember a white ocean i walked—each step darkening,
+until the path became ink.
+if i keep going, will it write me out?`
 	},
 	{
 		title: 'chronicles of a dream',
 		content: `a daydream: a tiny city atop a palette.
-		a shower of stardust waltzes, dancing in ink.
-		hide-and-seek in the oort’s seams.
-		i wander, aimless,
-		until the night lays me down—gently.`
+a shower of stardust waltzes, dancing in ink.
+hide-and-seek in the oort’s seams.
+i wander, aimless,
+until the night lays me down—gently.`
 	}
 ];
